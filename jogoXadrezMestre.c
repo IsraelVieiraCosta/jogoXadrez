9b39e2fc044ce18b4817cc8e05fda3d80878ca5f
IsraelVieiraCosta <99973442+IsraelVieiraCosta@users.noreply.github.com>
2025-09-16 19:42:53 -0300

    Update jogoXadrezMestre.c
    código criado

--- jogoXadrezMestre.c
+++ jogoXadrezMestre.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
 
+
+void recursivoTorre(int numero){
+	if (numero > 0){
+		printf("Direita \n");
+		
+		recursivoTorre(numero - 1);
+	}
+}
+
+void recursivoRainha(int numero){
+	if (numero > 0){
+		printf("Esquerda \n");
+		
+		recursivoRainha(numero - 1);
+	}
+}
+
+void recursivoBispo(int numero){
+	if (numero > 0){
+		printf("Cima, Direita \n");
+		
+		recursivoBispo(numero - 1);
+	}
+}
+
+void recursivoCavalo(int num){
+	if (num > 0){
+        for (int C = 1; C <= 2; C++){
+            printf("Cima, ");
+            }
+        printf("Direita\n");
+		
+		recursivoCavalo(num - 1);
+	}
+}
+
+int main(){
+
+    int torre = 5, rainha = 8, bispo = 5, cavalo = 1;
+
+    printf("\n");
+    recursivoTorre(torre);
+    printf("\n");
+    recursivoRainha(rainha);
+    printf("\n");
+    recursivoBispo(bispo);
+    printf("\n");
+    recursivoCavalo(cavalo);
+    printf("\n");
+    
+	return 0;
+}
